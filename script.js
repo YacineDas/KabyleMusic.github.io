735f7cf1d39b1bfd6c00dfa1086acfc1aa8adbce
YacineDas <94178590+YacineDas@users.noreply.github.com>
2023-04-09 14:44:13 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+const lyricsDiv = document.getElementById("lyrics");
+
+lyricsDiv.innerHTML = `
+  <h2>Yemma Yemma</h2>
+  <p>Yemma yemma, yemma a tulawin</p>
+  <p>Tasusmi ad tuffi d ameẓruy n ḥamzi</p>
+  <p>Yemma yemma, yemma a tulawin</p>
+  <p>Tasekkurt ad tuffi d ameẓruy n lḥeqq</p>
+`;
